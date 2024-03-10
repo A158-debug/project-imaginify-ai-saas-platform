c8eb6c21059a7efb317e56c201a59019f311b369
A158-debug <kumar.240@iitj.ac.in>
2024-03-10 12:00:53 +0530

    add initial setup for transformations

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationPage = () => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+
+  const transformationType = transformationTypes[type];
+
+  const { userId } = auth();
+  if (!userId) {
+    redirect('/sign-in');
+    return null;
+  }
+  const user = await getUserById(userId);
+
   return (
-    <div>AddTransformationPage</div>
+    <>
+      <Header
+        title={transformationType.title}
+        subtitle={transformationType.subTitle}
+      />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformationType.type as TransformationTypeKey}
+        creditBalance={user.creditBalance} />
+    </>
   )
 }
 
-export default AddTransformationPage
+export default AddTransformationTypePage
+
+// http://localhost:3000/transformatype 
